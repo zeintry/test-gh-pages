01580a622b4d5b232c21e623b026d969ec6c678d
zeintry <alanpo2000@hotmail.com>
2021-04-18 19:20:02 -0700

    Created dropdown for selecting graph type

--- docs/dropdown/dropdown.js
+++ docs/dropdown/dropdown.js
@@ -5,7 +5,7 @@ function myFunction() {
 }
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
+$(window).onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
