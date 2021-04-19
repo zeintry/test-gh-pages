ce4c551776594013156b833e1804cfc10015ade8
zeintry <alanpo2000@hotmail.com>
2021-04-18 20:34:48 -0700

    setting up choosing of graph

--- docs/dropdown/dropdown.js
+++ docs/dropdown/dropdown.js
@@ -1,19 +1,27 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function handleDrop() {
-  document.getElementById("graphDropdown").classList.toggle("show");
+    document.getElementById("graphDropdown").classList.toggle("show");
+}
+
+function handleGraphSelection(event) {
+    // get label to change text
+    var label = document.getElementById('graphLabel');
+    label.innerText = "Selected Graph: " + event.target.innerText;
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+    if (!event.target.matches('.dropbtn') || !event.taget.matches('a')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
 }
